9908b849577492c15856a8c894bc17936efdb1e5
Kram-Serg <bild003@yandex.ru>
2025-01-12 13:01:10 +0700

    Update views.py

--- task3/views.py
+++ task3/views.py
@@ -15,7 +15,20 @@ def buy_index(request):
     title = 'мой сайт'
     back = 'Вернуться на главную страницу'
     cart = 'Корзина'
-    context = {'title': title, 'back': back, 'cart': cart}
+    b_1 = 'Тонни'
+    b_2 = 'Яра'
+    b_3 = 'Эдна'
+    i_1 = 'Сьюзи'
+    i_2 = 'Генри'
+    i_3 = 'Элейн'
+    p_1 = 'Бадди'
+    p_2 = 'Роки'
+    p_3 = 'Кейли'
+    context = {'title': title, 'back': back, 'cart': cart,
+               'b_1': b_1, 'b_2': b_2,'b_3': b_3,
+               'i_1': i_1, 'i_2': i_2, 'i_3': i_3,
+               'p_1': p_1, 'p_2': p_2, 'p_3': p_3
+               }
     return render(request, 'buy.html', context)
 
 
